910952057621824a634d1022a35d259b938b05bd
SiddheshShinde-tech <siddhesh.shinde19@st.niituniversity.in>
2020-11-25 01:46:10 +0530

    Updates

---
+++ precache-manifest.76917cb7f75f34d5de332f456bba88f6.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968bb88f7bf3f34c9815eaf7c269f687",
+    "url": "/Soothe/index.html"
+  },
+  {
+    "revision": "0fc6f1ea46f6390043f8",
+    "url": "/Soothe/static/css/main.24707206.chunk.css"
+  },
+  {
+    "revision": "9543492d05d25f1fa91a",
+    "url": "/Soothe/static/js/2.89c15e4a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Soothe/static/js/2.89c15e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc6f1ea46f6390043f8",
+    "url": "/Soothe/static/js/main.3b6e0a50.chunk.js"
+  },
+  {
+    "revision": "2d0b83a6162b7fe3d82b",
+    "url": "/Soothe/static/js/runtime-main.c494171d.js"
+  },
+  {
+    "revision": "4536519a206b71e0840839c3e531a7f1",
+    "url": "/Soothe/static/media/Healthy2.4536519a.png"
+  },
+  {
+    "revision": "fd8ce54c4d8bce7509d9fdaab720c6ee",
+    "url": "/Soothe/static/media/Img.fd8ce54c.png"
+  },
+  {
+    "revision": "73d3ae501a5e402e525a7dae454f7460",
+    "url": "/Soothe/static/media/Mild2.73d3ae50.png"
+  },
+  {
+    "revision": "cdbab98282e7a46928a44473aea24596",
+    "url": "/Soothe/static/media/Objective.cdbab982.png"
+  },
+  {
+    "revision": "82003d9376daf35feec2ce3f2ddc316e",
+    "url": "/Soothe/static/media/annacover.82003d93.jpeg"
+  },
+  {
+    "revision": "acd8dcb7b3f1b9172c36ab2706438bc1",
+    "url": "/Soothe/static/media/assessment.acd8dcb7.jpg"
+  },
+  {
+    "revision": "1ec26ee070c0a549471a98a72ba5b5e5",
+    "url": "/Soothe/static/media/assessmentHome.1ec26ee0.jpg"
+  },
+  {
+    "revision": "b733805cbed22545454f3c1655cf347e",
+    "url": "/Soothe/static/media/b1.b733805c.jpeg"
+  },
+  {
+    "revision": "02e3750fb7d30d64b16b7438aeaec58d",
+    "url": "/Soothe/static/media/b2.02e3750f.jpg"
+  },
+  {
+    "revision": "a17b19c7d184ada89b549133dad41744",
+    "url": "/Soothe/static/media/b3.a17b19c7.jpg"
+  },
+  {
+    "revision": "f423a1103bbc4bad4c0a6ce79429875f",
+    "url": "/Soothe/static/media/b4.f423a110.jpg"
+  },
+  {
+    "revision": "15f67b76c899d7a08c62f990aa078e15",
+    "url": "/Soothe/static/media/b5.15f67b76.jpg"
+  },
+  {
+    "revision": "9e59096321d2667c2c4bc0a32cac4107",
+    "url": "/Soothe/static/media/blogs.9e590963.jpg"
+  },
+  {
+    "revision": "dfcb55ab4427a1205fde801e5a507e49",
+    "url": "/Soothe/static/media/blogsHome.dfcb55ab.jpg"
+  },
+  {
+    "revision": "070fa5eeb4e2baeba0fb6876f4770202",
+    "url": "/Soothe/static/media/chall2.070fa5ee.jpg"
+  },
+  {
+    "revision": "ebb171dc0b0dfec28bfa65952d9ef4d9",
+    "url": "/Soothe/static/media/depression-friend.ebb171dc.jpg"
+  },
+  {
+    "revision": "a3dac216ece0caa561914fedcefecfe2",
+    "url": "/Soothe/static/media/doctor-welcome.a3dac216.jpg"
+  },
+  {
+    "revision": "afdcc2c0a9b982df5812e027bee9d46a",
+    "url": "/Soothe/static/media/doctor4.afdcc2c0.JPG"
+  },
+  {
+    "revision": "1470a573e0bd9ddcc73b75dbd0418f7f",
+    "url": "/Soothe/static/media/doctors.1470a573.jpg"
+  },
+  {
+    "revision": "2b15751d7a0aeaa567a3bf923df9f6cc",
+    "url": "/Soothe/static/media/doctorsHome.2b15751d.jpg"
+  },
+  {
+    "revision": "8c5f2a3f2dddbeea1c32c0343dcddeac",
+    "url": "/Soothe/static/media/games.8c5f2a3f.jpg"
+  },
+  {
+    "revision": "8fd66329f980129bb294bfdf43166e56",
+    "url": "/Soothe/static/media/gamesHome.8fd66329.jpg"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/Soothe/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "95c4cfa87924cbd0f3237019f0b6cfd0",
+    "url": "/Soothe/static/media/logo5.95c4cfa8.png"
+  },
+  {
+    "revision": "aa516f83a4ada3cc4bd27bfe4f7f82bd",
+    "url": "/Soothe/static/media/logo6.aa516f83.png"
+  },
+  {
+    "revision": "9fa2ffd94d7e9c4975ce762727680d74",
+    "url": "/Soothe/static/media/medtrack.9fa2ffd9.jpg"
+  },
+  {
+    "revision": "2f0c82fca7b86710c0502c6ef56c0b4c",
+    "url": "/Soothe/static/media/mental-health-therapy.2f0c82fc.jpg"
+  },
+  {
+    "revision": "68132d9207ca2a52b58977c24f1fc9ae",
+    "url": "/Soothe/static/media/moderate2.68132d92.png"
+  },
+  {
+    "revision": "061318f5fff395337819f3e4ac31cf2b",
+    "url": "/Soothe/static/media/reso.061318f5.jpeg"
+  },
+  {
+    "revision": "1a4de98529a840d2a68f7fdb5a715915",
+    "url": "/Soothe/static/media/severe2.1a4de985.png"
+  },
+  {
+    "revision": "2dbc0389414a6aba562a8125e61f4bb6",
+    "url": "/Soothe/static/media/show_everyone.2dbc0389.jpeg"
+  },
+  {
+    "revision": "e7bd8b1a6004e56329ea6ee547cb3e8e",
+    "url": "/Soothe/static/media/temp-log.e7bd8b1a.jpg"
+  },
+  {
+    "revision": "7d64cf08861b0896d74e10852867aaf5",
+    "url": "/Soothe/static/media/therapies.7d64cf08.jpg"
+  },
+  {
+    "revision": "59ee2b478a7674c457ea9e0a267e7734",
+    "url": "/Soothe/static/media/therapyHome.59ee2b47.jpg"
+  },
+  {
+    "revision": "10521d53421493cfc30ec6a821b34a63",
+    "url": "/Soothe/static/media/trackHome.10521d53.jpg"
+  },
+  {
+    "revision": "e8ade151f9b7d2e02e75a51ec586356f",
+    "url": "/Soothe/static/media/video_image.e8ade151.jpeg"
+  }
+]);
