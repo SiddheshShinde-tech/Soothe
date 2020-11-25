292e0a75f567b9c238b60b0063f01fcd7e3302ca
SiddheshShinde-tech <siddhesh.shinde19@st.niituniversity.in>
2020-11-25 14:34:16 +0530

    HashRouter 1

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   // <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <ScrollToTop>
     <App />
     </ScrollToTop>
-    </BrowserRouter>,
+    </HashRouter>,
   // </React.StrictMode>,
   document.getElementById('root')
 );
