c172e31d4dd680f8a7798dad26aa0485ddd2de48
Siddhesh Shinde <62283380+SiddheshShinde-tech@users.noreply.github.com>
2020-11-26 18:47:24 +0530

    Logi Image updated
    https://ibb.co/B24c7rV

--- src/Components/login/logIn.js
+++ src/Components/login/logIn.js
@@ -34,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
   },
 //   backgroundImage: 'url(https://source.unsplash.com/random)',
   image: {
-    backgroundImage: 'url(https://i.ibb.co/4Mz6S19/temp.jpg)',
+    backgroundImage: 'url(https://ibb.co/B24c7rV)',
     backgroundRepeat: 'no-repeat',
     backgroundColor: theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
     backgroundSize: 'cover',
